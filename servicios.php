5ff5dd81c3607a79f9c4d93a4128b9141a015e21
mnaviaz <michelle.navia3@gmail.com>
2018-01-19 13:45:17 -0300

    responsivo

--- servicios.php
+++ servicios.php
@@ -27,11 +27,51 @@
 				<a href=""><p>Cortinas Roller</p><div class="cuadro-naranja"></div><img src="<?php echo get_template_directory_uri(); ?>/assets/tipo1.png" alt=""></a>	
 				</div>
 			</div>
-		</div>
+		</div>		
 		<div class="col-md-5">
 			<div class="foto-contexto wow fadeInUp">
 				<p class="texto-ambiente">Texto prueba</p>
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/ambiente.png" alt="">	
+			</div>
+		</div>
+
+	</div>
+</section>
+<section class="servicios2">
+	<div class="contendor-productos container">
+		<div class="row">
+			<div class="col-md-12">
+			<h1 class="wow zoomIn">Nuestros <span>Servicios</span></h1>
+			<p class="wow zoomIn">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa mollitia a neque labore</p>
+		</div>
+		</div>
+		
+		<div class="col-md-5">
+			<div class="foto-contexto wow fadeInUp">
+				<p class="texto-ambiente">Texto prueba</p>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/ambiente.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-7 col-sm-12">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contenedor-boton1  wow fadeInUp">
+				<a href=""><p>Cortinas Roller</p><div class="cuadro-naranja"></div><img src="<?php echo get_template_directory_uri(); ?>/assets/tipo1.png" alt=""></a>	
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contenedor-boton1  wow fadeInUp">
+				<a href=""><p>Cortinas Roller</p><div class="cuadro-naranja"></div><img src="<?php echo get_template_directory_uri(); ?>/assets/tipo1.png" alt=""></a>	
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contenedor-boton1  wow fadeInUp">
+				<a href=""><p>Cortinas Roller</p><div class="cuadro-naranja"></div><img src="<?php echo get_template_directory_uri(); ?>/assets/tipo1.png" alt=""></a>	
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contenedor-boton1  wow fadeInUp">
+				<a href=""><p>Cortinas Roller</p><div class="cuadro-naranja"></div><img src="<?php echo get_template_directory_uri(); ?>/assets/tipo1.png" alt=""></a>	
+				</div>
 			</div>
 		</div>
 	</div>
